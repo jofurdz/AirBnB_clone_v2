da42f5f9d41036ab20f5397183a801cdf8eb515f
jofurdz <jofurdz@gmail.com>
2021-11-25 00:05:22 -0600

    task 8 goof

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import os
 
 class City(BaseModel, Base):
