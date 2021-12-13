c0b42cd0d3cbadbe5335f3507d161c83f8b929ff
jofurdz <jofurdz@gmail.com>
2021-12-13 15:54:09 -0600

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -36,8 +36,17 @@ def poopla(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
-    return render_template('5-number.html', n = n)
+    return render_template('5-number.html')
 
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def num_parity(n):
+    if n % 2 == 0:
+        string = "even"
+    else:
+        string = "odd"
+    return render_template('6-number_odd_or_even.html', n = n, string = string)
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
