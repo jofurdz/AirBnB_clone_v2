f472b5a6fe39cf402ebbe1857c321a96fa5fc8ed
jofurdz <jofurdz@gmail.com>
2021-12-13 09:23:01 -0600

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""starts flask"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def ptext(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def poopla(n):
+    return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
