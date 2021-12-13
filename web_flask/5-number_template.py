099b8bdcb231868f3632da740b1c651b8ee494ee
jofurdz <jofurdz@gmail.com>
2021-12-13 15:26:03 -0600

    task 5 update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def poopla(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
-    return render_template('templates/5-number.html', H1)
+    return render_template('5-number.html')
 
 
 if __name__ == '__main__':
