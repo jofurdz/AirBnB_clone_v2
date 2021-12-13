8421bd876f6d50d265a37e27a12a880441818fab
jofurdz <jofurdz@gmail.com>
2021-12-13 09:18:31 -0600

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def C(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def ptext(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
